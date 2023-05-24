6ca35efcd11164b7e26115a71d6a68ef8ce6934e
Paolo Giovannini <giovannini13@gmail.com>
2023-05-24 14:54:16 +0200

    creata relazione e foreign key

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -19,5 +19,9 @@ class Project extends Model
     public static function generateSlug(string $title) {
         return Str::slug($title, '-');
     }
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
     
 }
